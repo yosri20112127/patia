8a434b928ac7e87db3bb623221491e60bde73200
humbert <humbert.fiorino@imag.fr>
2022-02-09 12:54:21 +0100

    Ajout gestion jar avec dépendances

---
+++ src/main/java/sokoban/Agent.java
@@ -0,0 +1,10 @@
+package sokoban;
+
+import java.util.Scanner;
+
+public class Agent {
+    public static void main(String[] args) {
+        String solution = "DUU";
+        for (char c : solution.toCharArray()) System.out.println(c);
+    }
+}
